8c7a39b725e84298f8ec0f2a89986b534fcb0004
StevenAFletcher <steven.fletcher8@outlook.com>
2020-11-03 00:31:25 +0900

    Initial Repository

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(AddUser i)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("MyAccount", i);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        [HttpGetAttribute ]
+        public IActionResult EditAccount()
+        {
+            AddUser j = new AddUser();
+            j.UserName = "Steven" ;
+            j.FirstName = "Steven";
+            j.LastName = "Flether";
+            j.Location = "Here";
+            j.Password = "Password";
+            j.EmailAddress = "steven.fletcher@example.com";
+            return View(j);
+        }   
+
+        
+        
+        
+
+        [HttpPostAttribute]
+        public IActionResult EditAccount(AddUser i)
+        {
+            return View("MyAccount");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Browse()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Project/Models/AddUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class AddUser
+    {
+        [Required(ErrorMessage = "User Name Is Required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "First Name Is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name Is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Is Required")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Location Is Required")]
+        public string Location { get; set; }
+            
+        }
+}
